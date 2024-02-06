c28ea5585309099069147bca50b3cee3be942006
Iago Richard <iagorichard@outlook.com>
2024-02-06 20:42:39 -0300

    Models

--- alexandria_library/models.py
+++ alexandria_library/models.py
@@ -1,6 +1,20 @@
+from datetime import datetime
 import uuid
 from sqlalchemy import Uuid
 from alexandria_library import db
 
 class User(db.Model):
     id = db.Column(Uuid, primary_key=True, default=uuid.uuid4)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    registerDate = db.Column(db.Date, nullable=False, default=datetime.today())
+    registerDate2 = db.Column(db.Date, nullable=False, default=datetime.today())
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
+    
+class Book(db.Model):
+    id = db.Column(Uuid, primary_key=True, default=uuid.uuid4)
+    name = db.Column(db.String(20), nullable=False)
+    author = db.Column(db.String(20), nullable=False)
