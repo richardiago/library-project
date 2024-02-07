717d91687f21b027fd70b53f01a8295ffc0b57e8
Iago Richard <iagorichard@outlook.com>
2024-02-07 20:51:30 -0300

    New tables

--- alexandria_library/models.py
+++ alexandria_library/models.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 import uuid
+from datetime import datetime
 from sqlalchemy import Uuid
 from alexandria_library import db
 
@@ -9,12 +9,36 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     registerDate = db.Column(db.Date, nullable=False, default=datetime.today())
-    registerDate2 = db.Column(db.Date, nullable=False, default=datetime.today())
 
     def __repr__(self):
         return f"User('{self.name}', '{self.email}')"
     
 class Book(db.Model):
     id = db.Column(Uuid, primary_key=True, default=uuid.uuid4)
-    name = db.Column(db.String(20), nullable=False)
-    author = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+    genre = db.Column(db.String(60), nullable=False)
+    publishDate = db.Column(db.Date, nullable=False)
+    registerDate = db.Column(db.Date, nullable=False, default=datetime.today())
+
+    genres = db.relationship('Genre', secondary='book_genre', backref=db.backref('books', lazy='dynamic'))
+
+    def __repr__(self):
+        return f"Book('{self.name}', '{self.author}')"
+    
+class Genre(db.Model):
+    id = db.Column(Uuid, primary_key=True, default=uuid.uuid4)
+    name = db.Column(db.String(80), nullable=False)
+    registerDate = db.Column(db.Date, nullable=False, default=datetime.today())
+
+    def __repr__(self):
+        return f"Genre('{self.name}')"
+    
+class BookGenre(db.Model):
+    id = db.Column(Uuid, primary_key=True, default=uuid.uuid4)
+    book_id = db.Column(Uuid, db.ForeignKey('book.id'), nullable=False)
+    genre_id = db.Column(Uuid, db.ForeignKey('genre.id'), nullable=False)
+    registerDate = db.Column(db.Date, nullable=False, default=datetime.today())
+
+    def __repr__(self):
+        return f"BookGenre('{self.book_id}', '{self.genre_id}')"
