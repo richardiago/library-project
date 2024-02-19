78a6534c9ab59f80c4cc1d35f17a2818d467e6a9
Iago Richard <iagorichard@outlook.com>
2024-02-19 15:10:24 -0300

    base html

--- migrations/versions/1e495b0573b8_.py
+++ migrations/versions/1e495b0573b8_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: cdeacdff36f7
+Revision ID: 1e495b0573b8
 Revises: 
-Create Date: 2024-02-15 22:23:23.600032
+Create Date: 2024-02-19 14:02:31.276924
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'cdeacdff36f7'
+revision = '1e495b0573b8'
 down_revision = None
 branch_labels = None
 depends_on = None
